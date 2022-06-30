61d2070ff539ad1b21d7500b72ba35405f8102c3
Nicolas Bologna <Nicolas.Bologna@endava.com>
2022-06-30 18:18:58 -0300

    remove template methods

--- ConsultaAlumnos.Presentacion/Controllers/StudentController.cs
+++ ConsultaAlumnos.Presentacion/Controllers/StudentController.cs
@@ -29,30 +29,5 @@ namespace ConsultaAlumnos.API.Controllers
 
             return _studentService.GetSubjectsByStudent(int.Parse(user)).ToList();
         }
-
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ValuesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
